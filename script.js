81638d7efe88c39a51a381eb7b7461e9ad1aea4b
Alexandre <alexandresbsantana@outlook.com>
2025-10-22 11:11:00 -0300

    Lógica do projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+// Programa para manipular o DOM
+window.document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btnMaiuscula").addEventListener("click", () => {
+    let inputText = document.getElementById("input-text").value;
+    document.getElementById("resultado").innerHTML = inputText.toUpperCase();
+  });
+});
+
+window.document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btnMinusculo").addEventListener("click", () => {
+    let inputText = document.getElementById("input-text").value;
+    document.getElementById("resultado").innerHTML = inputText.toLowerCase();
+  });
+});
