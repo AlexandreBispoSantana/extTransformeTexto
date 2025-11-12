7bf552ac983ca2d03a05b51b019a8743c94928da
alexandre CPTM <alexandresbsantana@outlook.com>
2025-11-12 09:11:52 -0300

    Projeto Transforme textos versão final

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 window.document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btnMaiuscula").addEventListener("click", () => {
     let inputText = document.getElementById("input-text").value;
-    document.getElementById("resultado").innerHTML = inputText.toUpperCase();
+    document.getElementById("input-resultado").innerHTML = inputText.toUpperCase();
   });
 });
 
 window.document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btnMinusculo").addEventListener("click", () => {
     let inputText = document.getElementById("input-text").value;
-    document.getElementById("resultado").innerHTML = inputText.toLowerCase();
+    document.getElementById("input-resultado").innerHTML = inputText.toLowerCase();
   });
 });
